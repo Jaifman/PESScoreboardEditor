15ba771a6559258817a07209fe9bc74460fc5616
Jaifman <jaifman@gmail.com>
2016-10-07 17:08:25 +0200

    Creation of Main Method class

---
+++ src/Editor/EditorMain.java
@@ -0,0 +1,15 @@
+package Editor;
+
+import javax.swing.JFrame;
+
+public class EditorMain {
+
+	public static void main(String[] args) {
+		
+		EditorFrame myFrame = new EditorFrame();
+		
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
